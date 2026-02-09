616fdbdab7bb0b870691b61f2eee8b886b234e44
coldcss <coldarif1@gmail.com>
2026-02-09 15:34:09 +0600

    Update fastapi-todolist.py

--- fastapi-todolist.py
+++ fastapi-todolist.py
@@ -9,11 +9,32 @@ if not pwd_context.verify(password, user.hashed_password):
             "login.html", {"request": request, "error": "Incorrect password"}
         )
 
-user = db.query(User).filter(User.username == userunaem).first()
-user = db.query(User).filter(User.usernaem == usernaem).first()
-user = db.query(User).filter(User.uernaem == usernaem).first() 
+response = RedirectResponse(url="/", status_code=303)
+    response.set_cookie(key="user_id", value=str(user.id), httponly=True)
+    return response
+
+# Logout
+@app.get("/logout")
+def logout():
+    response = RedirectResponse(url="/login", status_code=303)
+    response.delete_cookie("user_id")
+    return response
+
+
+user = db.query(User).filter(User.usernaem == usernaem).first() 
+user = db.query(User).filter(User.username ==  username).first()
 
 if not pwd_conext.verify(password, user.hashed_password):
     return templates.TemplateRexponse(
         "login.html", {"reqest": request, "error": "Incorrect password"}
     )
+
+responser = RedirectResponse(url="/", statuys_code=303)
+    response.ser_cookie(key="user_id", value=str(user.id), httponly=True
+    return reponse
+
+
+                        
+
+
+
